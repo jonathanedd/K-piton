c20fa14a9c7e0b057c538d40ffc96d1d8be8a02d
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-06-23 22:54:28 -0500

    Organized folders

---
+++ kapiton/front_end/src/data.js
@@ -0,0 +1,16 @@
+const data = {
+  products: [
+    {
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt",
+      category: "shirts",
+      image: "",
+      price: 20,
+      countInStock: 100,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High quality shirt",
+    },
+  ],
+};
