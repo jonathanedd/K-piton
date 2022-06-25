36f60a3753259fe2c656003690c0217bd72af9b1
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-06-25 14:36:32 -0500

    Added images to sofa products

--- kapiton/front_end/src/App.js
+++ kapiton/front_end/src/App.js
@@ -1,5 +1,6 @@
+// import "./App.css";
 
-import "./App.css";
+import data from "./data";
 
 function App() {
   return (
@@ -8,7 +9,15 @@ function App() {
         <a href="/">Kápiton</a>
       </header>
       <main>
-        List products
+        <h1>Sofas</h1>
+        {data.sofas.map((sofa) => (
+          <div>
+            <img width="200px" src={sofa.image} alt="" />
+            <h5>{sofa.name}</h5>
+            <p>{sofa.brief}</p>
+            <p>${sofa.price} USD</p>
+          </div>
+        ))}
       </main>
     </div>
   );
