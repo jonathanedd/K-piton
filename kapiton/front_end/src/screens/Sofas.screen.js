f145afb9187359c26c690afbd05c36571ad2ec41
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-06-25 22:06:20 -0500

    Added reducer

--- kapiton/front_end/src/screens/Sofas.screen.js
+++ kapiton/front_end/src/screens/Sofas.screen.js
@@ -1,14 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import axios from "axios";
+import logger from "use-reducer-logger";
 import { Link } from "react-router-dom";
 // import data from "../data";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, sofas: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
 const SofasScreen = () => {
-  const [sofas, setSofas] = useState([]);
+  const [{ loading, error, sofas }, dispatch] = useReducer(logger(reducer), {
+    sofas: [],
+    loading: true,
+    error: "",
+  });
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("/api/sofas");
-      setSofas(result.data);
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/sofas");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
     };
 
     fetchData();
@@ -19,23 +43,29 @@ const SofasScreen = () => {
       <main>
         <h1>Sofas</h1>
         <div className="sofas">
-          {sofas.map((sofa) => (
-            <div className="sofa" key={sofa.slug}>
-              <Link to={`/sofa/${sofa.slug}`}>
-                <img src={sofa.image} alt="" />
-              </Link>
-              <div className="sofa-info">
+          {loading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div>{error}</div>
+          ) : (
+            sofas.map((sofa) => (
+              <div className="sofa" key={sofa.slug}>
                 <Link to={`/sofa/${sofa.slug}`}>
-                  <h5>{sofa.name}</h5>
+                  <img src={sofa.image} alt="" />
                 </Link>
-                <p>
-                  <strong>${sofa.price} USD</strong>
-                </p>
-                {/* <p>{sofa.brief}</p> */}
-                <button>Add to cart</button>
+                <div className="sofa-info">
+                  <Link to={`/sofa/${sofa.slug}`}>
+                    <h5>{sofa.name}</h5>
+                  </Link>
+                  <p>
+                    <strong>${sofa.price} USD</strong>
+                  </p>
+                  {/* <p>{sofa.brief}</p> */}
+                  <button>Add to cart</button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </main>
     </div>
