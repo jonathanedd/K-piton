7296b32c7a8bb5978580de229ce942d325918d18
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-06-25 23:43:46 -0500

    Added react bootstrap

--- kapiton/front_end/src/App.js
+++ kapiton/front_end/src/App.js
@@ -2,15 +2,22 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/Home.screen";
 import Sofa from "./screens/Sofa.info";
-
 import SofasScreen from "./screens/Sofas.screen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <header>
-          <Link to="/">Kápiton</Link>
+          <Navbar bg="dark" variant="dark" />
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>Kápiton</Navbar.Brand>
+            </LinkContainer>
+          </Container>
         </header>
 
         <main>
