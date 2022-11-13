26641a1e3813dcea68adbfa142489c357a6361a0
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-11-13 01:35:27 -0500

    created css styles for placeOrderScreen

--- kapiton/front_end/src/screens/PlaceOrderScreen.js
+++ kapiton/front_end/src/screens/PlaceOrderScreen.js
@@ -3,6 +3,7 @@ import CheckOutSteps from "../components/checkout/CheckoutSteps";
 import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { Store } from "../Store";
+import "../styles/placeorderscreen.css";
 
 export default function PlaceOrderScreen() {
   const navigate = useNavigate();
@@ -27,77 +28,79 @@ export default function PlaceOrderScreen() {
   }, [cart, navigate]);
 
   return (
-    <div>
+    <>
       <CheckOutSteps step1 step2 step3 step4></CheckOutSteps>
-      <h1>Preview order</h1>
+      <div className="global-box">
+        <h1>Order preview</h1>
 
-      <div>
-        <h3>Shipping Information:</h3>
-        <ul>
-          <li>
-            <strong>Name:</strong> {cart.shippingAddress.fullName}
-          </li>
-          <li>
-            <strong>Address:</strong> {cart.shippingAddress.address},{" "}
-            {cart.shippingAddress.city},{cart.shippingAddress.postalCode},{" "}
-            {cart.shippingAddress.country}
-          </li>{" "}
-          <br />
-          <button>
-            <Link to="/shipping">Edit</Link>
-          </button>
-        </ul>
-      </div>
+        <div className="main-box-order-screen">
+          <div className="shipping-info">
+            <h3>Shipping Information:</h3>
+            <ul>
+              <li>
+                <strong>Name:</strong> {cart.shippingAddress.fullName}
+              </li>
+              <li>
+                <strong>Address:</strong> {cart.shippingAddress.address},{" "}
+                {cart.shippingAddress.city},{cart.shippingAddress.postalCode},{" "}
+                {cart.shippingAddress.country}
+              </li>{" "}
+              <br />
+              <button>
+                <Link to="/shipping">Edit</Link>
+              </button>
+            </ul>
+          </div>
 
-      <div>
-        <h3>Payment info:</h3>
-        <ul>
-          <li>{cart.paymentMethod}</li>
-        </ul>
-        <button>
-          <Link to="/payment">Edit</Link>
-        </button>
-      </div>
+          <div className="payment-info">
+            <h3>Payment info:</h3>
+            <ul>
+              <li>{cart.paymentMethod}</li>
+            </ul>
+            <button>
+              <Link to="/payment">Edit</Link>
+            </button>
+          </div>
 
-      <div>
-        <h3>Items:</h3>
-        {cart.cartItems.map((item) => (
-          <ul>
-            <li>
-              <img width="200px" src={item.image} alt={item.name} />
-            </li>
-            <li>
-              <Link to={`/product/${item.slug}`}>{item.name}</Link>
-            </li>
-            <li>
-              <span>{item.quantity}</span>
-            </li>
-            <li>${item.price}</li>
-          </ul>
-        ))}
-        <button>
-          <Link to="/cart">Edit</Link>
-        </button>
-      </div>
+          <div className="items">
+            <h3>Items:</h3>
+            {cart.cartItems.map((item) => (
+              <ul>
+                <img src={item.image} alt={item.name} />
+                <li>
+                  <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                </li>
+                <li>
+                  <span>{item.quantity}</span>
+                </li>
+                <li>${item.price}</li>
+              </ul>
+            ))}
+            <button>
+              <Link to="/cart">Edit</Link>
+            </button>
+          </div>
 
-      <div>
-        <h3>Order summary</h3>
-        <ul>
-          <li>Items: ${cart.itemsPrice.toFixed(2)}</li>
-          <li>Shipping: ${cart.shippingPrice.toFixed(2)}</li>
-          <li>Tax: ${cart.taxPrice.toFixed(2)}</li>
-          <li>
-            <strong>${cart.totalPrice.toFixed(2)}</strong>
-          </li>
-        </ul>
-        <button
-          type="button"
-          onClick={placeOrderHandler}
-          disabled={cart.cartItems.length === 0}
-        >
-          Place order
-        </button>
+          <div className="order-summary">
+            <h3>Order summary</h3>
+            <ul>
+              <li>Items: ${cart.itemsPrice.toFixed(2)}</li>
+              <li>Shipping: ${cart.shippingPrice.toFixed(2)}</li>
+              <li>Tax: ${cart.taxPrice.toFixed(2)}</li>
+              <li>
+                <strong>Total: ${cart.totalPrice.toFixed(2)}</strong>
+              </li>
+            </ul>
+            <button
+              type="button"
+              onClick={placeOrderHandler}
+              disabled={cart.cartItems.length === 0}
+            >
+              Place order
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
